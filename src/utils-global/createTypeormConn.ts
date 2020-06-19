2a4dbeec4c56fe45f6e7cbe9491f96acbd2ec243
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-19 13:29:17 -0500

    try to deploy6

--- src/utils-global/createTypeormConn.ts
+++ src/utils-global/createTypeormConn.ts
@@ -1,12 +1,31 @@
 import { getConnectionOptions, createConnection } from "typeorm";
+// import * as PostgressConnectionStringParser from "pg-connection-string";
 
 export const createTypeormConnection = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return process.env.NODE_ENV === "production"
-    ? createConnection({
-        ...connectionOptions,
-        url: process.env.DATABASE_URL,
-        name: "default",
-      } as any)
-    : createConnection({ ...connectionOptions, name: "default" });
+  // const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  // return process.env.NODE_ENV === "production"
+  //   ? createConnection({
+  //       type: "postgres",
+  //       url: process.env.DATABASE_URL,
+  //       synchronize: true,
+  //       logging: true,
+  //     } as any)
+  //   : createConnection({ ...connectionOptions });
+  console.log(process.env.DATABASE_URL);
+  if (process.env.DATABASE_URL) {
+    console.log(process.env.DATABASE_URL);
+    // const connectionOptions = PostgressConnectionStringParser.parse(
+    //   process.env.DATABASE_URL!
+    // );
+    createConnection({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      extra: {
+        ssl: true,
+      },
+    } as any);
+  } else {
+    const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+    createConnection({ ...connectionOptions });
+  }
 };
