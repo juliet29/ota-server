adac42c48fa7ad47c1f417b698e41774cb38f4a2
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-19 15:36:13 -0500

    try to deploy11

--- src/utils-global/createSchema.ts
+++ src/utils-global/createSchema.ts
@@ -1,8 +1,23 @@
+import { GetCurrentUserResolver } from "src/modules/user/GetCurrentUser";
 import { buildSchema } from "type-graphql";
+import { ChangePasswordResolver } from "../modules/user/ChangePassword";
+import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
+import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
+import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
+import { RegisterResolver } from "../modules/user/Register";
 
 export const CreateSchema = () =>
   buildSchema({
-    resolvers: [__dirname + "/../modules/*/*.ts"],
+    resolvers: [
+      ChangePasswordResolver,
+      ConfirmUserResolver,
+      ForgotPasswordResolver,
+      LoginResolver,
+      LogoutResolver,
+      GetCurrentUserResolver,
+      RegisterResolver,
+    ],
     // userId cookie must be available on session to access authorized resolvers
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
