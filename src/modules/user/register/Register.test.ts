8c75406b3ca37e1a8e4c152a05facd600ce471c1
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-19 07:32:29 -0500

    actually testing test

--- src/modules/user/register/Register.test.ts
+++ src/modules/user/register/Register.test.ts
@@ -1,6 +1,8 @@
 import { Connection } from "typeorm";
 import { testConn } from "../../../test-utils/testConn";
 import { gCall } from "../../../test-utils/gCall";
+import faker from "faker";
+import { User } from "../../../entity/User";
 
 let conn: Connection;
 
@@ -26,17 +28,38 @@ mutation Register($data: RegisterInput!) {
 
 describe("Register", () => {
   it("create user", async () => {
-    console.log(
-      await gCall({
-        source: registerMutation,
-        variableValues: {
-          data: {
-            username: "newuser2",
-            email: "bob@bob.com",
-            password: "asdfasdf",
-          },
+    const user = {
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const response = await gCall({
+      source: registerMutation,
+      variableValues: {
+        data: user,
+      },
+    });
+
+    // make a graphql call
+    if (response.errors) {
+      console.log(response.errors[0].originalError);
+    }
+
+    // expect the response of the graphql to have this format
+    expect(response).toMatchObject({
+      data: {
+        register: {
+          username: user.username,
+          email: user.email,
         },
-      })
-    );
+      },
+    });
+
+    // expect the unconfirmed user to be stored correctly in the database
+    const dbUser = await User.findOne({ where: { email: user.email } });
+    expect(dbUser).toBeDefined();
+    expect(dbUser!.confirmed).toBeFalsy();
+    expect(dbUser!.username).toBe(user.username);
   });
 });
