edf4c9a58d1eb2aeed0982fc3ff74d8f07c85700
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-08-27 10:49:29 -0400

    get posts of followers for one type

--- src/modules/post/GetPosts.ts
+++ src/modules/post/GetPosts.ts
@@ -1,10 +1,20 @@
-import { Arg, createUnionType, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  createUnionType,
+  Query,
+  Resolver,
+  UseMiddleware,
+  Ctx,
+} from "type-graphql";
 // import { isAuth } from "../middleware/isAuth";
 import { AlbumPost, ArtistPost, TrackPost } from "../../entity/ContentPosts";
 import { createQueryBuilder } from "typeorm";
 import { User } from "../../entity/User";
 import { Poll } from "../../entity/Poll";
 import { Playlist } from "../../entity/Playlist";
+import { isAuth } from "../middleware/isAuth";
+import { MyContext } from "src/types/MyContext";
+import { AuthenticationError } from "apollo-server-express";
 
 export const GetPostsResultUnion = createUnionType({
   name: "GetPostsResult",
@@ -81,6 +91,41 @@ export class GetPostsResolver {
       ...polls,
       ...playlists,
     ];
+  }
+
+  @UseMiddleware(isAuth)
+  @Query(() => [GetPostsResultUnion])
+  async getPostsOfFollowing(@Ctx() ctx: MyContext) {
+    // get the user from the context
+    const user = await User.findOne(ctx.payload?.userId)!;
+    if (!user) {
+      throw new AuthenticationError("User not found");
+    }
+
+    const query = createQueryBuilder().relation(User, "albumPost");
+
+    // get posts based on id of people this user follows
+    const exQuery = async (k: number) => {
+      return await query.of(k).loadMany();
+    };
+
+    const ids = [17, 19];
+
+    const getData = async () => {
+      return Promise.all(ids.map(async (i) => exQuery(i)));
+    };
+    // let allPosts: any[] = [];
+    const posts = await getData().then((data) => {
+      // const newData = data.map((i) => Object.values(i));
+      const hello: any[] = [];
+      data.forEach((i) => i.forEach((k) => hello.push(k)));
+      console.log("\n ALL posts", hello, "\n");
+      return hello;
+    });
+
+    // console.log("\n ALL posts", posts, "\n");
+
+    return [...posts];
   }
 
   @Query(() => [ArtistPost])
